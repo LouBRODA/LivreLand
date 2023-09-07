a8b644e716fc880c4f2941a38d78d88ca94eceec
lobroda <lou.broda@etu.uca.fr>
2023-09-07 14:24:03 +0100

    ADD : image cover book and more view :zap:

--- LivreLand/View/TousView.xaml.cs
+++ LivreLand/View/TousView.xaml.cs
@@ -6,12 +6,12 @@ public partial class TousView : ContentPage
 {
 	public List<BookModel> AllBooks { get; set; } = new List<BookModel>()
 	{
-		new BookModel("Alain Damasio","La horde du contrevent","Non lu", 0),
-		new BookModel("Alain Damasio",".La zone du dehors","Terminé", 0),
-		new BookModel("Cixin Liu","L'équateur d'Einstein","Non lu", 0)
+		new BookModel("La horde du contrevent","Alain Damasio","Non lu", 0),
+		new BookModel("La zone du dehors","Alain Damasio","Terminé", 0),
+		new BookModel("L'équateur d'Einstein","Cixin Liu","Non lu", 0)
     };
 
-	public TousView()
+    public TousView()
 	{
 		BindingContext = this;
 		InitializeComponent();
