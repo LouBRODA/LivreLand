f719a59f7cd9f440b6ef8ff4dcbf4ea743eed259
lobroda <lou.broda@etu.uca.fr>
2023-09-16 20:45:19 +0100

    ADD : bug dark color, nice bookmark position

--- LivreLand/View/ContentViews/EmpruntsPretsButtonView.xaml.cs
+++ LivreLand/View/ContentViews/EmpruntsPretsButtonView.xaml.cs
@@ -9,17 +9,38 @@ public partial class EmpruntsPretsButtonView : ContentView
 
 	public void OnPretsClicked(object sender, EventArgs e)
 	{
-		pretsClicked.BackgroundColor = Colors.Transparent;
-		pretsClicked.IsEnabled = false;
-		empruntsButton.BackgroundColor = Colors.White;
-		empruntsButton.IsEnabled = true;
+		if (App.Current.PlatformAppTheme == AppTheme.Light)
+		{
+			pretsClicked.BackgroundColor = Colors.Transparent;
+			pretsClicked.IsEnabled = false;
+			empruntsButton.BackgroundColor = Colors.White;
+			empruntsButton.IsEnabled = true;
+		}
+		else
+		{
+            pretsClicked.BackgroundColor = Colors.Transparent;
+            pretsClicked.IsEnabled = false;
+            empruntsButton.BackgroundColor = Colors.Black;
+            empruntsButton.IsEnabled = true;
+        }
 	}
 
     public void OnEmpruntsClicked(object sender, EventArgs e)
     {
-        pretsClicked.BackgroundColor = Colors.White;
-        pretsClicked.IsEnabled = true;
-        empruntsButton.BackgroundColor = Colors.Transparent;
-        empruntsButton.IsEnabled = false;
+		if (App.Current.PlatformAppTheme == AppTheme.Light)
+		{
+			pretsClicked.BackgroundColor = Colors.White;
+			pretsClicked.IsEnabled = true;
+			empruntsButton.BackgroundColor = Colors.Transparent;
+			empruntsButton.IsEnabled = false;
+		}
+		else
+		{
+            pretsClicked.BackgroundColor = Colors.Black;
+            pretsClicked.IsEnabled = true;
+            empruntsButton.BackgroundColor = Colors.Transparent;
+            empruntsButton.IsEnabled = false;
+        }
+
     }
 }
